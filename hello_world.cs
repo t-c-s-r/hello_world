1c9226fbb8545b723a555e048b9d335904f45275
Thea Rogerson <theacscottrogerson@gmail.com>
2018-10-29 16:51:06 +0000

    adding

---
+++ hello_world.cs
@@ -0,0 +1,17 @@
+namespace SampleNamespace
+{
+    class SampleClass
+    {
+        public void SampleMethod()
+        {
+            System.Console.WriteLine(
+              "SampleMethod inside SampleNamespace");
+        }
+    }
+}
+static void Main()/*this is how you write main functions in c# */
+{
+    //...
+}
+
+/*This is how you write comments*/
