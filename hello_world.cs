86ad5fc318ae4e2f24b185f01ad48d3f97788b6f
Thea Rogerson <theacscottrogerson@gmail.com>
2018-10-29 21:47:32 +0000

    updated hello world function and more git testing

---
+++ hello_world.cs
@@ -0,0 +1,34 @@
+using System;
+namespace helloWorld{/* is namespace declarartion?*/
+
+public class Hello/* a class?*/
+{
+    public static void Main()/*a main method*/
+    {
+        System.Console.WriteLine("Hello, World!");/*statement*/
+    }
+}
+}
+
+// /* a main method */
+// /* expressions need to be assigned? */
+
+// /* notes below */
+// namespace SampleNamespace
+// {
+//     class SampleClass
+//     {
+//         public void SampleMethod()
+//         {
+//             // a comment line
+//             System.Console.WriteLine(
+//               "Hello world");
+//         }
+//     }
+// }
+
+// static void Main()/*this is how you write main functions in c# */
+// {
+//     //...
+// }
+
