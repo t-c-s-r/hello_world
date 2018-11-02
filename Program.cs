4a708d080e3c3ac866b64e3d8d1f16fc72a6a515
t-c-s-r <43747247+t-c-s-r@users.noreply.github.com>
2018-11-02 11:31:41 +0000

    Hello world programme with help from another student
    Hey, we found another student who has experience with c# to talk us through this. If this isn't right please give us details on why not because we are out of ideas!

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hello_World
+{
+    //class
+    class Program
+    {
+        static void Main(string[] args)//class attributes - void means it doesn't return anything, all methods have to declare what they will return
+        {
+            //class method
+            string helloWorldString = "Hello World";
+            PrintString(helloWorldString);
+            Console.ReadKey();//This stops the programme from closing
+        }
+        static void PrintString (string input)
+        {
+            //main method
+            Console.WriteLine(input);
+        }
+    }
+}
